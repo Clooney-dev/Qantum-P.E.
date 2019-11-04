1da3c727a16332272ec427f5c168a5bad2b3560c
Andrey <Andrey@DESKTOP-4FIOCUF>
2019-11-04 19:28:14 +0300

    Добавьте файлы проекта.

---
+++ Урок1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+class Newton
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите массу тела: ");
+        var m = Convert.ToInt32(Console.ReadLine());
+        #region Проверка
+       
+        
+            if (m <= 0)
+            {
+            try
+            {
+                throw new ArgumentNullException();
+            }
+            catch (ArgumentNullException) { Console.WriteLine("Вес не может быть меньше или равен нулю"); }
+
+               
+
+            }
+        else
+        {
+            Console.WriteLine("Всё правильно");
+
+        }
+        #endregion
+        Console.WriteLine();
+        var c = 300000000;
+        var c1 = Convert.ToInt64(Math.Pow(c, 2));
+
+
+
+        var E = m * c1;
+        Console.WriteLine(E);
+        Console.ReadLine();
+
+
+
+
+
+    }
+} 
+
+
+
+
+     
